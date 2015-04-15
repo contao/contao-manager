fafc0792c3872f98001d1c0ac757dd01aed2d2c6
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-04-15 02:41:03 +0200

    Add compile task and change require to require-dev.

---
+++ src/Compiler/UiTask.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * This file is part of tenside/ui.
+ *
+ * (c) Christian Schiffler <https://github.com/discordier>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    tenside/ui
+ * @author     Christian Schiffler <https://github.com/discordier>
+ * @copyright  Christian Schiffler <https://github.com/discordier>
+ * @link       https://github.com/tenside/ui
+ * @license    https://github.com/tenside/ui/blob/master/LICENSE MIT
+ * @filesource
+ */
+
+namespace Tenside\Ui\Compiler;
+
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Process\Process;
+use Tenside\Compiler\AbstractTask;
+
+/**
+ * This Compiler task adds all content from tenside/ui into the phar.
+ *
+ * @author Christian Schiffler <https://github.com/discordier>
+ */
+class UiTask extends AbstractTask
+{
+    /**
+     * Run the passed process and add every line from the output to the log.
+     *
+     * @param Process $process The process to execute.
+     *
+     * @return void
+     */
+    private function runProcess(Process $process)
+    {
+        $process->setTimeout(null)->mustRun();
+        foreach (explode(PHP_EOL, $process->getOutput()) as $line) {
+            $this->notice($line);
+        }
+    }
+
+    /**
+     * Compile the tenside/ui assets.
+     *
+     * @param string $compiledAssets The destination path for the assets.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
+     */
+    private function compileUi($compiledAssets)
+    {
+        $uiDir = $this->getPackageRoot('tenside/ui');
+
+        if (!is_dir($compiledAssets)) {
+            mkdir($compiledAssets, 0755, true);
+        }
+        $compiledAssets = realpath($compiledAssets);
+        $this->notice('Performing npm install...');
+        $process = new Process('npm install --save-dev', $uiDir);
+        $this->runProcess($process);
+
+        $this->notice('Performing gulp install...');
+        $process = new Process($uiDir . '/node_modules/.bin/gulp install', $uiDir);
+        $this->runProcess($process);
+        $this->notice('Performing gulp build...');
+        $process = new Process(
+            $uiDir . '/node_modules/.bin/gulp build',
+            $uiDir,
+            array_merge(
+                $_SERVER,
+                [
+                    'DEST_DIR' => $compiledAssets,
+                    'TENSIDE_VERSION' => $this->getVersionInformationFor('tenside/core', 'version'),
+                    'COMPOSER_VERSION' => $this->getVersionInformationFor('composer/composer', 'version'),
+                ]
+            )
+        );
+        $this->runProcess($process);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Add tenside/ui to the phar file.
+     */
+    public function compile()
+    {
+        $compiledAssets = dirname(dirname(__DIR__)) . '/.build';
+
+        $this->compileUi($compiledAssets);
+
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->notName('UiTask.php')
+            ->in(dirname(dirname(__DIR__)) . '/src');
+        foreach ($finder as $file) {
+            $this->addFile($file);
+        }
+
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->ignoreVCS(true)
+            ->name('*.css')
+            ->name('*.js')
+            ->name('*.map')
+            ->name('*.png')
+            ->name('*.svg')
+            ->name('*.html')
+            ->name('*.otf')
+            ->name('*.eot')
+            ->name('*.ttf')
+            ->name('*.woff')
+            ->in($compiledAssets);
+        foreach ($finder as $file) {
+            /** @var SplFileInfo $file */
+            $this->addFile($file, false, str_replace($compiledAssets, 'assets', $file->getRealPath()));
+        }
+    }
+}
---
+++ src/Web/Controller/UiController.php
@@ -0,0 +1,179 @@
+<?php
+
+/**
+ * This file is part of tenside/ui.
+ *
+ * (c) Christian Schiffler <https://github.com/discordier>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    tenside/ui
+ * @author     Christian Schiffler <https://github.com/discordier>
+ * @copyright  Christian Schiffler <https://github.com/discordier>
+ * @link       https://github.com/tenside/ui
+ * @license    https://github.com/tenside/ui/blob/master/LICENSE MIT
+ * @filesource
+ */
+
+namespace Tenside\Ui\Web\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Tenside\Web\Controller\AbstractController;
+
+/**
+ * Provide access to all embedded asset files.
+ */
+class UiController extends AbstractController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function createRoutes(RouteCollection $routes)
+    {
+        static::createRoute($routes, 'rootRedirect', '/', __CLASS__);
+        static::createRoute($routes, 'index', '/index.html', __CLASS__);
+
+        static::createRoute(
+            $routes,
+            'asset',
+            '/{path}/{file}',
+            __CLASS__,
+            ['GET'],
+            [
+                'path' => 'css|fonts|img|js|pages|tenside',
+                'file' => '[\-\_a-zA-Z]*(\.[a-zA-Z]*)+'
+            ]
+        );
+    }
+
+    /**
+     * Redirect any request to the "/" path to the index.html file.
+     *
+     * @param Request $request The request to process.
+     *
+     * @return RedirectResponse
+     */
+    public function rootRedirectAction(Request $request)
+    {
+        $uri = $request->getUri();
+        return new RedirectResponse($uri . 'index.html');
+    }
+
+    /**
+     * Provide the index.html file.
+     *
+     * @param Request $request The request to process.
+     *
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        return new Response(
+            str_replace(
+                'var TENSIDEApi=window.location.href.split(\'#\')[0];',
+                'var TENSIDEApi=\'' . $request->getSchemeAndHttpHost() . $request->getBaseUrl() . '/\';',
+                file_get_contents($this->getAssetsDir() . '/index.html')
+            ),
+            200,
+            array(
+                'Content-Type' => 'text/html; charset=UTF-8'
+            )
+        );
+    }
+
+    /**
+     * Provide an asset.
+     *
+     * @param string $path The assets sub dir.
+     *
+     * @param string $file The file name within the sub dir..
+     *
+     * @return Response
+     */
+    public function assetAction($path, $file)
+    {
+        $filePath = $this->getAssetsDir() . '/' . $path . '/' . $file;
+
+        if (file_exists($filePath)) {
+            $response = new Response(file_get_contents($filePath));
+            $mime     = $this->getMime($filePath);
+            if ($mime !== null) {
+                $response->headers->set('Content-Type', $mime);
+            }
+
+            return $response;
+        }
+
+        return new Response($filePath . ' not found', 404);
+    }
+
+    /**
+     * Retrieve the mime type of a file.
+     *
+     * @param string $filePath The filename for which the mime shall be guessed.
+     *
+     * @return string|null
+     */
+    private function getMime($filePath)
+    {
+        if (substr($filePath, -3) === '.js') {
+            return 'text/javascript; charset=UTF-8';
+        }
+
+        if (substr($filePath, -5) === '.html') {
+            return 'text/html; charset=UTF-8';
+        }
+
+        if (substr($filePath, -4) === '.css') {
+            return 'text/css; charset=UTF-8';
+        }
+
+        if (substr($filePath, -4) === '.png') {
+            return 'image/png';
+        }
+
+        if (substr($filePath, -4) === '.svg') {
+            return 'image/svg+xml';
+        }
+
+        if (substr($filePath, -5) === '.woff') {
+            return 'application/font-woff';
+        }
+
+        if (substr($filePath, -4) === '.map') {
+            return 'application/json';
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieve the assets dir.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException When the assets dir can not be located.
+     */
+    private function getAssetsDir()
+    {
+        if ($phar = \Phar::running()) {
+            return $phar . '/assets';
+        }
+
+        // FIXME: hardcoded assets path for non phar mode - change this!
+        $dir = dirname(__DIR__);
+        while (($dir = dirname($dir)) !== '.') {
+            if (is_dir($dir . '/.build')) {
+                return $dir . '/.build';
+            }
+        }
+
+        throw new \RuntimeException('Could not find assets directory.');
+    }
+}
