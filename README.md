bf6a66c6e986093c864c58f2226828b11a23e018
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-12-15 15:43:13 +0100

    Add test environment and activate travis

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is the contao edition of tenside
-=====================================
+This is the contao package manager
+==================================
 
-This glues the ui and core of tenside together.
+[![Version](http://img.shields.io/packagist/v/contao/package-manager.svg?style=flat-square)](https://packagist.org/packages/contao/package-manager)
+[![Stable Build Status](http://img.shields.io/travis/contao/package-manager/master.svg?style=flat-square&label=stable%20build)](https://travis-ci.org/contao/package-manager)
+[![License](http://img.shields.io/packagist/l/contao/package-manager.svg?style=flat-square)](http://spdx.org/licenses/MIT)
---
+++ bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of tenside/core.
+ *
+ * (c) Christian Schiffler <c.schiffler@cyberspectrum.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    tenside/core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2015 Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @license    https://github.com/tenside/core/blob/master/LICENSE MIT
+ * @link       https://github.com/tenside/core
+ * @filesource
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if (
+    // Locally installed dependencies
+    (!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
+    // We are within an composer install.
+    && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
+}
