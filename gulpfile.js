c55e2595070cecd0ea1f4a1ce4936db2b688136a
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-03-03 14:17:18 +0100

    Added foundation for standalone scripts that are not globally needed

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,43 @@ const sourcemaps    = require('gulp-sourcemaps');
 const cleanCSS      = require('gulp-clean-css');
 const uglify        = require('gulp-uglify');
 
-var scriptsGlob  = 'assets/js/*.js';
-var stylesGlob   = 'assets/css/*.scss';
+var combinedScriptsGlob     = 'assets/js/*.js';
+var standaloneScriptsGlob   = 'assets/js/components/*.js';
+var combinedStylesGlob      = 'assets/css/*.scss';
 
 
 // Build javascripts task
 gulp.task('scripts', function() {
-    gulp.src(scriptsGlob)
+
+    // Combined
+    gulp.src(combinedScriptsGlob)
         .pipe(babel({
             presets: ['react']
         }))
         .pipe(browserify())
-        .pipe(concat('scripts.js'))
+        .pipe(concat('global.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('web'));
+        .pipe(gulp.dest('web/js'));
+
+    // Standalone
+    gulp.src(standaloneScriptsGlob)
+        .pipe(babel({
+            presets: ['react']
+        }))
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('web/js'));
 });
 
 // Build SASS task
 gulp.task('sass', function () {
-    return gulp.src(stylesGlob)
+    return gulp.src(combinedStylesGlob)
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
-        .pipe(concat('styles.css'))
+        .pipe(concat('global.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('web'));
+        .pipe(gulp.dest('web/css'));
 });
 
 // Build by default
