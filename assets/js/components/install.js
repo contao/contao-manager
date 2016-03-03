e071b12f0abf09f4e6625396b56f8ca4d41fa003
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-03-03 16:19:39 +0100

    Added password compare routine

--- assets/js/components/install.js
+++ assets/js/components/install.js
@@ -3,16 +3,44 @@ var ReactDOM = require('react-dom');
 
 var Widget = React.createClass({
     render: function() {
+
+        var onChange = null;
+        if (undefined !== this.props.onChange){
+            onChange = this.props.onChange.bind(null, this.props);
+        }
+
         return (
-            <div className="widget">
-                <label htmlFor="ctrl_{this.props.name}">{this.props.label}</label>
-                <input type="{this.props.type}" id="ctrl_{this.props.name}" placeholder="{this.props.placeholder}" />
+            <div className={"widget" + (this.props.error ? " error" : "")}>
+                <label htmlFor={this.props.name}>{this.props.label}</label>
+                <input type={this.props.type} id={"ctrl_" + this.props.name} placeholder={this.props.placeholder} onChange={onChange} />
             </div>
         );
     }
 });
 
 var InstallComponent = React.createClass({
+    password: '',
+    passwordConfirm: '',
+    getInitialState: function() {
+        return {
+            passwordsMatch: true
+        };
+    },
+
+    handlePasswordCompare: function(props, e) {
+        if (props.name == 'password') {
+            this.password = e.target.value;
+        } else {
+            this.passwordConfirm = e.target.value;
+        }
+
+        if (this.password === this.passwordConfirm) {
+            this.setState({passwordsMatch: true});
+        } else {
+            this.setState({passwordsMatch: false});
+        }
+    },
+
     render: function() {
         return (
             <form action="#" method="post">
@@ -21,8 +49,8 @@ var InstallComponent = React.createClass({
                     <p>Create a user account to manage your installation.</p>
 
                     <Widget type="text" name="username" label="Username"></Widget>
-                    <Widget type="password" name="password" label="Password"></Widget>
-                    <Widget type="password" name="password_confirm" label="Retype Password"></Widget>
+                    <Widget type="password" name="password" label="Password" onChange={this.handlePasswordCompare} error={!this.state.passwordsMatch}></Widget>
+                    <Widget type="password" name="password_confirm" label="Retype Password" onChange={this.handlePasswordCompare} error={!this.state.passwordsMatch}></Widget>
 
                 </fieldset>
 
@@ -30,7 +58,7 @@ var InstallComponent = React.createClass({
                     <legend>Contao Installation</legend>
                     <p>Enter a version to install or leave blank for the latest version.</p>
 
-                    <Widget type="text" name="version" label="Retype Password" placeholder="latest"></Widget>
+                    <Widget type="text" name="version" label="Version" placeholder="latest"></Widget>
 
                 </fieldset>
 
