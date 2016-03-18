56905cf1aabe9a1db51efc3ac0937cbb04023bea
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-03-18 20:53:03 +0100

    Added a state component to check for the current tenside state

---
+++ assets/js/components/tenside/state.js
@@ -0,0 +1,22 @@
+const Promise       = require('promise');
+const jQuery        = require('jquery');
+
+
+module.exports = {
+    getState: function() {
+        return new Promise(function (resolve, reject) {
+            jQuery.ajax('/api/v1/install/get_state', {
+                dataType: 'json'
+            }).success(function (response) {
+                if ('OK' === response.status) {
+                    resolve(response.state)
+                } else {
+                    reject(response);
+                }
+
+            }).fail(function (err) {
+                reject(err);
+            });
+        });
+    }
+};
