8d5f3a22519ec0d09dad5a83f62103f42ab86c14
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-04 15:39:29 +0200

    Always redirect to install route if tenside is either not configured or not installed at all yet

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,25 @@ const hashchange   = require('hashchange');
 const React        = require('react'); // has to be present here because JSX is transformed to React.createElement()
 const ReactDOM     = require('react-dom');
 const App          = require('./components/app.js');
+const TensideState = require('./components/tenside/state.js');
 
-
+// Routing definition
 function handleRoute(hash) {
-    var app = <App route={hash} />;
-    ReactDOM.render(app, document.getElementById('app'));
+
+    // Check Tenside state and redirect to install screen if not installed
+    TensideState.getState()
+        .then(function(state) {
+            // If not configured or project not created, go to the install screen
+            if (true !== state.tenside_configured || true !== state.project_created) {
+                hash = 'install';
+                hashchange.updateHash('install');
+            }
+
+            var app = <App route={hash} />;
+            ReactDOM.render(app, document.getElementById('app'));
+        });
 }
 
 hashchange.update(handleRoute);
 hashchange.update();
+
