357aacd13ca083c1a935592109082cf862d5d82d
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-04 15:59:03 +0200

    Do not reload to remove flickering and redirect to login screen when tenside configured but no project was created yet

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,21 +3,33 @@
 const hashchange   = require('hashchange');
 const React        = require('react'); // has to be present here because JSX is transformed to React.createElement()
 const ReactDOM     = require('react-dom');
+const history      = require('history').createHistory();
 const App          = require('./components/app.js');
 const TensideState = require('./components/tenside/state.js');
+const request      = require('./components/request.js');
 
 // Routing definition
 function handleRoute(hash) {
 
-    // Check Tenside state and redirect to install screen if not installed
+    // Check Tenside state
     TensideState.getState()
         .then(function(state) {
-            // If not configured or project not created, go to the install screen
-            if (true !== state.tenside_configured || true !== state.project_created) {
+            // If not configured, go to the install screen
+            if (true !== state.tenside_configured) {
                 hash = 'install';
-                hashchange.updateHash('install');
+                updateHashWithoutRedirect(hash);
             }
 
+            return state;
+        })
+        .then(function(state) {
+            // If no project was created and not logged in, go to the login screen
+            if (true !== state.project_created && '' === request.getToken()) {
+                hash = 'login';
+                updateHashWithoutRedirect(hash);
+            }
+        })
+        .then(function() {
             var app = <App route={hash} />;
             ReactDOM.render(app, document.getElementById('app'));
         });
@@ -26,3 +38,6 @@ function handleRoute(hash) {
 hashchange.update(handleRoute);
 hashchange.update();
 
+function updateHashWithoutRedirect(hash) {
+    history.push(window.location.pathname + '#' + hash);
+}
