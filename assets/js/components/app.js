36589771f8ae4cd1a887c1d3d0cd89c5f6029bab
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-05 18:52:00 +0200

    Preparing packages

--- assets/js/components/app.js
+++ assets/js/components/app.js
@@ -3,14 +3,17 @@
 const React         = require('react');
 const Install       = require('./public/install.js');
 const Login         = require('./public/login.js');
+const Packages      = require('./manager/packages.js');
 
 var AppComponent = React.createClass({
 
     render: function() {
-
         switch (this.props.route)  {
             case 'install':
                 return <Install />;
+
+            case 'packages':
+                return <Packages />;
 
             default:
                 return <Login />;
