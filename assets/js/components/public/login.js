75fd182d8d4fc32bea634feb74ea273280126040
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-05 11:33:59 +0200

    Implemented the login screen functionality

--- assets/js/components/public/login.js
+++ assets/js/components/public/login.js
@@ -1,31 +1,107 @@
 'use strict';
 
 const React         = require('react');
-const Trappings     = require('./trappings.js')
+const jQuery        = require('jQuery');
+const Trappings     = require('./trappings.js');
+const Translation   = require('../translation.js');
 const TextWidget    = require('../widgets/text.js');
+const request       = require('../helpers/request.js');
 
 
 var InstallComponent = React.createClass({
     getInitialState: function() {
-        return {};
+        return {
+            isLoggingIn: false,
+            isLoggedIn: false
+        }
+    },
+
+    componentDidMount: function() {
+
+        if ('' !== request.getUsername() && '' !== request.getToken()) {
+            this.setState({isLoggedIn: true});
+        }
+    },
+
+    handleLogin: function(e) {
+        e.preventDefault();
+        this.setState({isInstalling: true});
+
+        var self = this;
+        var form = jQuery('#login-form');
+        var username = form.find('input[name="username"]').first().val();
+        var password = form.find('input[name="password"]').first().val();
+
+        this.login(username, password)
+            .then(function() {
+                self.setState({isLoggedIn: true});
+            })
+    },
+
+    login: function(username, password) {
+        return new Promise(function (resolve, reject) {
+
+            var authPayload = {
+                username: username,
+                password: password
+            };
+
+            request.createRequest('/api/v1/auth', {
+                method: 'POST',
+                data: JSON.stringify(authPayload),
+                dataType: 'json'
+            }).success(function (response) {
+                if ('OK' === response.status) {
+                    // Store the JWT
+                    request.setToken(response.token);
+                    request.setUsername(username);
+
+                    resolve(response);
+                } else {
+                    reject(response);
+                }
+            }).fail(function (err) {
+                reject(err);
+            });
+        });
     },
 
     render: function() {
-        return (
-            <Trappings sectionClass="login">
-                <h1>Sign In</h1>
-                <p>Login to manage your installation.</p>
 
-                <form action="#" method="post">
-                    <TextWidget type="text" name="username" label="Username" placeholder="Username" />
-                    <TextWidget type="password" name="password" label="Password" placeholder="Password" />
+        var username = request.getUsername();
+        var disabled = this.state.isLoggingIn || this.state.isLoggedIn;
 
-                    <a href="">Forgot your password?</a>
+        if (this.state.isLoggedIn) {
+            var translationPlaceholders = { username: request.getUsername() };
 
-                    <button type="submit">Sign In</button>
-                </form>
-            </Trappings>
-        );
+            return (
+                <Trappings sectionClass="login">
+                    <h1><Translation placeholders={translationPlaceholders}>You are logged in as %username%.</Translation></h1>
+                </Trappings>
+            );
+        } else {
+            return (
+                <Trappings sectionClass="login">
+                    <h1>Sign In</h1>
+                    <p>Login to manage your installation.</p>
+
+                    <form id="login-form" action="#" method="post">
+                        <TextWidget type="text" name="username" label="Username"
+                                    placeholder="Username"/>
+                        <TextWidget type="password" name="password"
+                                    label="Password" placeholder="Password"/>
+
+
+                        {/* @todo Implement a forgot password functionality? */}
+                        {/* <a href="">Forgot your password?</a> */}
+
+                        <button disabled={disabled} type="submit"
+                                onClick={this.handleLogin}>Sign in
+                        </button>
+                    </form>
+                </Trappings>
+            );
+        }
     }
 });
 
