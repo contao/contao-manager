57456497025dbca376cd589e3f7708735d7b4d46
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-14 18:34:51 +0200

    Added a getLoggedIn() check to tenside state

--- assets/js/components/helpers/tenside-state.js
+++ assets/js/components/helpers/tenside-state.js
@@ -1,22 +1,42 @@
-const Promise       = require('bluebird');
-const jQuery        = require('jquery');
+'use strict';
 
+const request       = require('./request.js');
+const Promise       = require('bluebird');
+
+var getState = function() {
+    return new Promise(function (resolve, reject) {
+        request.createRequest('/api/v1/install/get_state', {
+            dataType: 'json'
+        }).success(function (response) {
+            if ('OK' === response.status) {
+                resolve(response.state)
+            } else {
+                reject(response);
+            }
+
+        }).fail(function (err) {
+            reject(err);
+        });
+    });
+};
+
+var getLoggedIn = function() {
+    return new Promise(function (resolve, reject) {
+        request.createRequest('/api/v1/auth', {
+            dataType: 'json'
+        }).success(function (response) {
+            if ('OK' === response.status) {
+                resolve({user_loggedIn: true});
+            } else {
+                resolve({user_loggedIn: false});
+            }
+        }).fail(function () {
+            resolve({user_loggedIn: false});
+        });
+    });
+};
 
 module.exports = {
-    getState: function() {
-        return new Promise(function (resolve, reject) {
-            jQuery.ajax('/api/v1/install/get_state', {
-                dataType: 'json'
-            }).success(function (response) {
-                if ('OK' === response.status) {
-                    resolve(response.state)
-                } else {
-                    reject(response);
-                }
-
-            }).fail(function (err) {
-                reject(err);
-            });
-        });
-    }
+    getState: getState,
+    getLoggedIn: getLoggedIn
 };
