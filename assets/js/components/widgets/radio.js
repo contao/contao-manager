fa17ec0f27f6d3584415555140636bff9f2b98ee
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-15 20:55:16 +0200

    Added a radio widget

---
+++ assets/js/components/widgets/radio.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const React = require('react');
+
+var Option = React.createClass({
+
+    handleChange: function(e) {
+        if (undefined !== this.props.onChange){
+            this.props.onChange.call(this, this.props, e);
+        }
+    },
+
+    render: function() {
+        return (
+            <span>
+                <input
+                    type="radio"
+                    name={this.props.name}
+                    id={this.props.id}
+                    value={this.props.value}
+                    checked={this.props.selected}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor={this.props.id}>{this.props.label}</label>
+            </span>
+        )
+    }
+});
+
+var Widget = React.createClass({
+
+    getInitialState: function() {
+        return {
+            selected: this.props.selected
+        };
+    },
+
+    handleChange: function(e) {
+        this.setState({
+            selected: e.value
+        });
+
+        if (undefined !== this.props.onChange){
+            this.props.onChange.call(this, this.props, e);
+        }
+    },
+
+    render: function() {
+
+        var name = this.props.name;
+        var self = this;
+
+        var options = [];
+        this.props.options.map(function(option, index) {
+            options.push(<Option
+                name={name}
+                key={name + '_' + index}
+                id={name + '_' + index}
+                value={option.value}
+                label={option.label}
+                selected={option.value === self.state.selected}
+                onChange={self.handleChange}
+            />);
+        });
+
+        return (
+            <fieldset>
+                <legend>{this.props.label}</legend>
+                {options}
+            </fieldset>
+        )
+    }
+});
+
+module.exports = Widget;
