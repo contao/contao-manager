662c1ac3d7a441f039da41685747664dee6f2d43
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-24 15:19:31 +0200

    Simplified state promises

--- assets/js/components/helpers/tenside-state.js
+++ assets/js/components/helpers/tenside-state.js
@@ -1,39 +1,30 @@
 'use strict';
 
 const request       = require('./request.js');
-const Promise       = require('bluebird');
 
 var getState = function() {
-    return new Promise(function (resolve, reject) {
-        request.createRequest('/api/v1/install/get_state')
-            .then(function (response) {
-                if ('OK' === response.status) {
-                    resolve(response.state)
-                } else {
-                    reject(new Error(response));
-                }
 
-            })
-            .catch(function (err) {
-                reject(new Error(err));
-            });
-    });
+    return request.createRequest('/api/v1/install/get_state')
+        .then(function (response) {
+            if ('OK' === response.status) {
+                return response.state;
+            }
+        });
 };
 
 var getLoggedIn = function() {
-    return new Promise(function (resolve, reject) {
-        request.createRequest('/api/v1/auth')
-            .then(function (response) {
-                if ('OK' === response.status) {
-                    resolve({user_loggedIn: true, username: response.username});
-                } else {
-                    resolve({user_loggedIn: false});
-                }
-            })
-            .catch(function () {
-                resolve({user_loggedIn: false});
-            });
-    });
+
+    return request.createRequest('/api/v1/auth')
+        .then(function (response) {
+            if ('OK' === response.status) {
+                return {user_loggedIn: true, username: response.username};
+            } else {
+                return {user_loggedIn: false};
+            }
+        })
+        .catch(function () {
+            return {user_loggedIn: false};
+        });
 };
 
 module.exports = {
