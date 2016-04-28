e8cad9d5d9dbd949b915ecf6f884826c95928212
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-04-28 16:16:03 +0200

    Never index a shit

--- src/Resources/views/index.html.php
+++ src/Resources/views/index.html.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Contao Package Manager</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <link rel="stylesheet" href="<?= $css ?>">
 </head>
 <body>
