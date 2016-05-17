57e3b8ffe2ecc13737da0b50e326dacab75f330b
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-05-17 17:36:52 +0200

    Reorganize routing helper a little

--- assets/js/helpers/routing.js
+++ assets/js/helpers/routing.js
@@ -6,100 +6,93 @@ const forIn        = require('lodash/forIn');
 const request      = require('./request.js');
 
 // Route definitions (do not define dynamically so they get bundled by browserify)
-var routeDefinitions = {
-    'install':          require('./../routes/install.js'),
-    'login':            require('./../routes/login.js'),
-    'packages':         require('./../routes/packages.js'),
-    'app-kernel':       require('./../routes/app-kernel.js'),
-    'composer-json':    require('./../routes/composer-json.js'),
-    'self-test':        require('./../routes/self-test.js'),
-    'logout':           require('./../routes/logout.js')
-};
-var _initialized = false;
-var router = null;
-var routes = [];
-var currentRoute = null;
+var Routing = {
+    routeDefinitions: {
+        'install': require('./../routes/install.js'),
+        'login': require('./../routes/login.js'),
+        'packages': require('./../routes/packages.js'),
+        'app-kernel': require('./../routes/app-kernel.js'),
+        'composer-json': require('./../routes/composer-json.js'),
+        'self-test': require('./../routes/self-test.js'),
+        'logout': require('./../routes/logout.js')
+    },
 
-var _initialize = function() {
-    if (_initialized) {
-        return;
+    _initialized: false,
+    router: null,
+    routes: [],
+    currentRoute: null,
+
+    _initialize: function () {
+        var self = this;
+        if (this._initialized) {
+            return;
+        }
+
+        // Router
+        this.router = crossroads.create();
+
+        forIn(this.routeDefinitions, function (routeDef, routeName) {
+            var route = self.router.addRoute(routeDef.path);
+            route.name = routeName;
+            route.preController = routeDef.preController;
+            route.controller = routeDef.controller;
+            self.routes[routeName] = route;
+        });
+
+        this._initialized = true;
+    },
+
+    getRouter: function () {
+        this._initialize();
+        return this.router;
+    },
+
+    getRoutes: function () {
+        this._initialize();
+        return this.routes;
+    },
+
+    getRoute: function (routeName) {
+        this._initialize();
+        var routes = this.getRoutes();
+        return routes[routeName];
+    },
+
+    generateUrl: function (routeName, lang) {
+        this._initialize();
+        lang = typeof lang !== 'undefined' ? lang : this.getLanguage();
+        var route = this.getRoute(routeName);
+        return route.interpolate({locale: lang});
+    },
+
+    isCurrentRoute: function (routeName) {
+        return routeName === currentRoute;
+    },
+
+    setCurrentRoute: function (routeName) {
+        this.currentRoute = routeName;
+    },
+
+    redirect: function (routeName, lang) {
+        this._initialize();
+        history.push(this.generateUrl(routeName, lang));
+    },
+
+    getHistory: function () {
+        return history;
+    },
+
+    getLanguage: function () {
+        var lang = 'en';
+
+        if (undefined !== document
+            && undefined !== document.getElementsByTagName('html')[0]
+            && undefined !== document.getElementsByTagName('html')[0].lang) {
+            lang = document.getElementsByTagName('html')[0].lang;
+        }
+
+        return lang;
     }
-
-    // Router
-    router = crossroads.create();
-
-    forIn(routeDefinitions, function(routeDef, routeName) {
-        var route = router.addRoute(routeDef.path);
-        route.name = routeName;
-        route.preController = routeDef.preController;
-        route.controller  = routeDef.controller;
-        routes[routeName] = route;
-    });
-
-    _initialized = true;
 };
 
-var getRouter = function()
-{
-    _initialize();
-    return router;
-};
-
-var getRoutes = function() {
-    _initialize();
-    return routes;
-};
-
-var getRoute = function(routeName) {
-    _initialize();
-    var routes = getRoutes();
-    return routes[routeName];
-};
-
-var generateUrl = function (routeName, lang) {
-    _initialize();
-    lang = typeof lang !== 'undefined' ? lang : getLanguage();
-    var route = getRoute(routeName);
-    return route.interpolate({locale: lang});
-};
-
-var isCurrentRoute = function(routeName) {
-    return routeName === currentRoute;
-};
-
-var setCurrentRoute = function(routeName) {
-    currentRoute = routeName;
-};
-
-var redirect = function(routeName, lang) {
-    _initialize();
-    history.push(generateUrl(routeName, lang));
-};
-
-var getHistory = function() {
-    return history;
-};
-
-var getLanguage = function() {
-    var lang = 'en';
-
-    if (undefined !== document
-        && undefined !== document.getElementsByTagName('html')[0]
-        && undefined !== document.getElementsByTagName('html')[0].lang) {
-        lang = document.getElementsByTagName('html')[0].lang;
-    }
-
-    return lang;
-};
-
-module.exports = {
-    getRouter: getRouter,
-    getRoutes: getRoutes,
-    getRoute: getRoute,
-    generateUrl: generateUrl,
-    isCurrentRoute: isCurrentRoute,
-    setCurrentRoute: setCurrentRoute,
-    redirect: redirect,
-    getHistory: getHistory,
-    getLanguage: getLanguage
-};
+module.exports = Routing;
