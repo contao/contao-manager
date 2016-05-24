e618831925ddb6aa764061e1f4d5ace9696e60ae
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-05-24 14:13:04 +0200

    Delete orphan tasks (older than a week) no matter the state and type

--- assets/js/helpers/taskmanager.js
+++ assets/js/helpers/taskmanager.js
@@ -2,22 +2,58 @@
 
 const eventhandler  = require('./eventhandler.js');
 const request       = require('./request.js');
+const forIn         = require('lodash/forIn');
 
-var runNextTask = function() {
-
-    return request.createRequest('/api/v1/tasks/run')
-        .then(function (response) {
-            if ('OK' === response.status) {
-                eventhandler.emit('displayTaskPopup', {
-                    taskId: response.task
-                });
-            }
-        })
-        .catch(function (err) {
-            // @todo
-        });
-};
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
 
 module.exports = {
-    runNextTask: runNextTask
+    runNextTask: function() {
+        return request.createRequest('/api/v1/tasks/run')
+            .then(function (response) {
+                if ('OK' === response.status) {
+                    eventhandler.emit('displayTaskPopup', {
+                        taskId: response.task
+                    });
+                }
+            })
+            .catch(function (err) {
+                // @todo
+            });
+    },
+
+    deleteTask: function(taskId) {
+        return request.createRequest('/api/v1/tasks/' + taskId, {
+            method: 'DELETE'
+        });
+    },
+
+    getTaskList: function() {
+        return request.createRequest('/api/v1/tasks');
+    },
+
+    deleteOrphanTasks: function() {
+        // Delete tasks older than a week
+        var now = new Date();
+        var self = this;
+
+        this.getTaskList()
+            .then(function(result) {
+                result['foobar'] = {
+                    created_at: "2016-05-22T14:50:54+0000"
+                };
+
+                forIn(result, function(data, taskId) {
+                    var createdAt = Date.parse(data['created_at']);
+                    var compare = addDays(createdAt, 7);
+
+                    if (compare < now) {
+                        self.deleteTask(taskId);
+                    }
+                });
+            });
+    }
 };
--- assets/js/routes/packages.js
+++ assets/js/routes/packages.js
@@ -5,11 +5,13 @@ const ReactDOM      = require('react-dom');
 const App           = require('./../components/app.js');
 const Packages      = require('./../components/routes/packages.js');
 const routeChecks   = require('./../helpers/common-route-checks.js');
+const taskmanager   = require('./../helpers/taskmanager.js');
 
 module.exports = {
     path: '/{locale}/packages',
     preController: [routeChecks.ifTensideNotOkRedirectToInstall, routeChecks.ifUserNotLoggedInRedirectToLogin],
     controller: function(request, routing) {
+        taskmanager.deleteOrphanTasks();
         ReactDOM.render(<App routing={routing}><Packages /></App>, document.getElementById('app'));
     }
 };
