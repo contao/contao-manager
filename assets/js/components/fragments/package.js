6b83e73ad4d74a4eed5befb1ffc8ab6592b1223f
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-06-24 10:37:21 +0200

    Show hint when installing packages

--- assets/js/components/fragments/package.js
+++ assets/js/components/fragments/package.js
@@ -58,7 +58,7 @@ var PackageComponent = React.createClass({
         this.setState({
             removed: true,
             enabled: this.props.enabled,
-            constraint: ''
+            constraint: false
         });
     },
 
@@ -84,6 +84,7 @@ var PackageComponent = React.createClass({
         e.preventDefault();
         this.setState({
             removed: false,
+            installed: false,
             enabled: this.props.enabled,
             constraint: this.initialConstraint
         });
@@ -99,7 +100,13 @@ var PackageComponent = React.createClass({
     },
 
     getHintMessageData: function() {
-        if (this.state.removed) {
+        if (this.state.installed) {
+            if (null !== this.state.constraint) {
+                return ['This package will be installed with constraint "%constraint%" when you apply the changes.', {constraint: this.state.constraint}];
+            } else {
+                return ['This package will be installed in the best available version when you apply the changes.'];
+            }
+        } else if (this.state.removed) {
             return ['This package will be removed when you apply the changes.'];
         } else if (this.state.enabled !== this.props.enabled && this.state.constraint !== this.initialConstraint) {
             if (this.state.enabled) {
@@ -121,7 +128,7 @@ var PackageComponent = React.createClass({
             }
         } else if (this.state.enabled !== this.props.enabled) {
             return this.state.enabled ? ['This package will be enabled when you apply the changes.'] : ['This package will be disabled when you apply the changes.'];
-        } else if (this.state.constraint !== this.initialConstraint) {
+        } else if (this.state.constraint !== this.initialConstraint && null !== this.initialConstraint) {
             return ['The constraint for this package will be changed from "%old_constraint%" to "%new_constraint%" when you apply the changes.',
                 {
                     old_constraint: this.initialConstraint,
@@ -140,11 +147,13 @@ var PackageComponent = React.createClass({
             hint = <HintComponent handleRevert={this.handleRevert}><Translation domain="package" placeholders={hintData[1]}>{hintData[0]}</Translation></HintComponent>
         }
 
-        if ('packages' === this.props.mode) {
+        if (('packages' === this.props.mode && false !== this.state.constraint ) || this.state.installed) {
             release = (
                 <ReleaseComponent
                     initialConstraint={this.initialConstraint}
                     constraint={this.state.constraint}
+                    placeholder={'search' === this.props.mode ? 'latest version' : ''}
+                    mode={this.props.mode}
                     onConstraintChange={this.handleConstraintChange}
                 />
             );
@@ -203,8 +212,7 @@ var ActionsComponent = React.createClass({
         if (this.props.canBeInstalled) {
             buttons.push(
                 <button key="install" className="install" onClick={this.props.onInstall} disabled={this.props.isInstalled}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90.8 105.6"><path d="M0 8.3h31.8V0H59v8.3h31.8v12.5H0V8.3zm8.3 17.8h75.8v79.5H8.3m64-70.7h-7.6v62.8h7.5l.1-62.8zm-22.7 0h-7.5v62.8h7.5V34.9zm-22.8.5h-7.5v62.8h7.5V35.4z"/></svg>
-                    &nbsp;Install
+                    Check &amp; Install
                 </button>
             );
         }
@@ -272,7 +280,13 @@ var ReleaseComponent = React.createClass({
     },
 
     componentDidUpdate: function(prevProps, prevState) {
-        this.refs.constraintInput && this.refs.constraintInput.focus();
+        if (this.refs.constraintInput) {
+            if (this.state.constraintInputDisabled) {
+                this.refs.constraintInput.blur();
+            } else {
+                this.refs.constraintInput.focus();
+            }
+        }
 
         if (this.state.constraintInputDisabled && this.state.constraint != this.props.constraint) {
             this.setState({constraint: this.props.constraint});
@@ -308,6 +322,13 @@ var ReleaseComponent = React.createClass({
 
     validateConstraint: function() {
         var self = this;
+
+        if ('' === this.state.constraint || null === this.state.constraint) {
+            self.setState({constraint: self.props.initialConstraint, constraintInputDisabled: true});
+            self.fireConstraintChangeEvent(self.props.initialConstraint);
+            return;
+        }
+
         this.setState({validating: true});
 
         request.createRequest('/api/v1/constraint', {
@@ -330,14 +351,10 @@ var ReleaseComponent = React.createClass({
     },
 
     render: function() {
-        if (this.state.constraint == '') {
-            return (<div></div>);
-        }
-
         return (
             <div className={'release' + (this.state.validating ? ' validating' : '')}>
                 <fieldset>
-                    <input type="text" ref="constraintInput" value={this.state.constraint} onBlur={this.handleConstraintBlur} onChange={this.handleConstraintChange} onKeyUp={this.handleConstraintKey} disabled={this.state.constraintInputDisabled} />
+                    <input type="text" ref="constraintInput" value={this.state.constraint} placeholder={this.props.placeholder} onBlur={this.handleConstraintBlur} onChange={this.handleConstraintChange} onKeyUp={this.handleConstraintKey} disabled={this.state.constraintInputDisabled} />
                     <button onClick={this.handleEnableConstraintInput}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
                             <path d="M88.1 50c0-1.2-.1-2.4-.2-3.5l12.1-7c-.7-3.4-1.8-6.7-3.2-9.8L82.9 31c-1.2-2-2.7-4-4.3-5.7l5.6-12.5c-2.6-2.3-5.5-4.3-8.5-6.1l-10.5 9.1c-2.2-.9-4.5-1.7-6.9-2.2L55.3.3a47.08 47.08 0 0 0-10.6 0l-3 13.3c-2.4.5-4.7 1.3-6.9 2.2L24.3 6.7c-3.1 1.7-5.9 3.8-8.5 6.1l5.6 12.5c-1.6 1.8-3 3.7-4.3 5.7L3.2 29.7C1.8 32.8.7 36.1 0 39.5l12.1 6.9c-.1 1.2-.2 2.3-.2 3.5s.1 2.4.2 3.5L0 60.5c.7 3.4 1.8 6.7 3.2 9.8L17.1 69c1.2 2 2.7 4 4.3 5.7l-5.6 12.5c2.6 2.3 5.5 4.3 8.5 6.1l10.5-9.1c2.2.9 4.5 1.7 6.9 2.2l3 13.3a47.08 47.08 0 0 0 10.6 0l3-13.3c2.4-.5 4.7-1.3 6.9-2.2l10.5 9.1c3.1-1.7 5.9-3.8 8.5-6.1l-5.6-12.5c1.6-1.8 3-3.7 4.3-5.7l13.9 1.3c1.4-3.1 2.5-6.4 3.2-9.8l-12.1-6.9c.2-1.2.2-2.4.2-3.6zM50 66.4c-9.2 0-16.7-7.3-16.7-16.4 0-9 7.5-16.4 16.7-16.4S66.7 40.9 66.7 50c0 9-7.5 16.4-16.7 16.4z"/>
