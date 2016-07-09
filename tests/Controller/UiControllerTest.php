d0c442b57b6df7bd43fa50a3c8a3a9b0ed0bf506
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-07-09 03:36:17 +0200

    Add UiController tests

--- tests/Controller/UiControllerTest.php
+++ tests/Controller/UiControllerTest.php
@@ -20,6 +20,8 @@
 
 namespace AppBundle\Test;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -71,5 +73,211 @@ class UiControllerTest extends WebTestCase
 
         $this->assertTrue($response->isRedirect($url));
         $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /**
+     * Test the index action.
+     *
+     * @return void
+     */
+    public function testUnknownRedirectsToIndex()
+    {
+        $client = $this->createClient();
+        $client->request('GET', 'https://tenside.example.com/does-not-exist', [], [], []);
+
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isRedirect('https://tenside.example.com/'));
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /**
+     * Test the app action.
+     *
+     * @return void
+     */
+    public function testAppAction()
+    {
+        $client   = $this->createClient();
+        $crawler  = $client->request('GET', 'https://tenside.example.com/de');
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertEquals('de', $crawler->filterXPath('//html')->attr('lang'));
+        $this->assertEquals('/', $crawler->filterXPath('//base')->attr('href'));
+        $this->assertEquals('/web-assets/css/bundle.css', $crawler->filterXPath('//link')->attr('href'));
+        $this->assertEquals('/web-assets/js/bundle.js', $crawler->filterXPath('//script')->attr('src'));
+    }
+
+    /**
+     * Test the app action (from within a phar file).
+     *
+     * @return void
+     */
+    public function testAppActionFromPharFile()
+    {
+        $client   = $this->createClient();
+        $crawler  = $client->request(
+            'GET',
+            'https://tenside.example.com/contao-manager.phar/de',
+            [],
+            [],
+            ['SCRIPT_FILENAME' => '/var/www/web/contao-manager.phar',]
+        );
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertEquals(
+            'de',
+            $crawler->filterXPath('//html')->attr('lang')
+        );
+        $this->assertEquals(
+            '/contao-manager.phar/',
+            $crawler->filterXPath('//base')->attr('href')
+        );
+        $this->assertEquals(
+            '/contao-manager.phar/web-assets/css/bundle.css',
+            $crawler->filterXPath('//link')->attr('href')
+        );
+        $this->assertEquals(
+            '/contao-manager.phar/web-assets/js/bundle.js',
+            $crawler->filterXPath('//script')->attr('src')
+        );
+    }
+
+    /**
+     * Provide the file paths for the asset action tests.
+     *
+     * @return array
+     */
+    public function assetActionProvider()
+    {
+        return [
+            [
+                'file' => 'css/bundle.css',
+                'mime' => 'text/css'
+            ],
+            [
+                'file' => 'js/bundle.js',
+                'mime' => 'text/javascript'
+            ],
+            [
+                'file' => 'images/logo.svg',
+                'mime' => 'image/svg+xml'
+            ],
+        ];
+    }
+
+    /**
+     * Test the asset action.
+     *
+     * @param string $file         The file to request.
+     *
+     * @param string $expectedMime The mime type that shall be reported.
+     *
+     * @return void
+     *
+     * @dataProvider assetActionProvider
+     */
+    public function testAssetAction($file, $expectedMime)
+    {
+        $fullPath = realpath(__DIR__ . '/../../src/Resources/public/' . $file);
+
+        if (!is_file($fullPath)) {
+            $this->markTestSkipped('File ' . $fullPath . ' could not be found, missing asset?');
+            return;
+        }
+
+        $client = $this->createClient();
+
+        $client->request('GET', 'https://tenside.example.com/web-assets/' . $file);
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(BinaryFileResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertEquals($fullPath, $response->getFile()->getPathname());
+        $this->assertEquals($expectedMime, $response->headers->get('Content-Type'));
+    }
+
+    /**
+     * Test the asset action (from within a phar file).
+     *
+     * @param string $file         The file to request.
+     *
+     * @param string $expectedMime The mime type that shall be reported.
+     *
+     * @return void
+     *
+     * @dataProvider assetActionProvider
+     */
+    public function testAssetActionFromPharFile($file, $expectedMime)
+    {
+        $fullPath = realpath(__DIR__ . '/../../src/Resources/public/' . $file);
+
+        if (!is_file($fullPath)) {
+            $this->markTestSkipped('File ' . $fullPath . ' could not be found, missing asset?');
+            return;
+        }
+
+        $client = $this->createClient();
+
+        $client->request(
+            'GET',
+            'https://tenside.example.com/contao-manager.phar/web-assets/' . $file,
+            [],
+            [],
+            ['SCRIPT_FILENAME' => '/var/www/web/contao-manager.phar',]
+        );
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(BinaryFileResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertEquals($fullPath, $response->getFile()->getPathname());
+        $this->assertEquals($expectedMime, $response->headers->get('Content-Type'));
+    }
+
+    /**
+     * Test that the translation action returns a message catalog.
+     *
+     * @return void
+     */
+    public function testTranslationAction()
+    {
+        $client = $this->createClient();
+
+        $client->request('GET', 'https://tenside.example.com/translation/de/install');
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+        $content = json_decode($response->getContent(), true);
+        $this->assertTrue(is_array($content));
+        $this->assertNotEmpty($content);
+    }
+
+    /**
+     * Test that the translation action returns an error for unknown catalog.
+     *
+     * @return void
+     */
+    public function testTranslationActionForUnknownCatalog()
+    {
+        $client = $this->createClient();
+
+        $client->request('GET', 'https://tenside.example.com/translation/invalid-locale!/do-not-care');
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals(
+            ['status' => 'ERROR', 'message' => 'Could not load translation'],
+            json_decode($response->getContent(), true)
+        );
     }
 }
