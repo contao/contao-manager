ae4998ce686f6531f5e6c9d492fc4d62e8e32de9
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-10-25 11:32:52 +0200

    Blur out boxed trappings when console opens

--- assets/js/components/trappings/boxed.js
+++ assets/js/components/trappings/boxed.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import eventhandler from '../../helpers/eventhandler';
 
-export default function(props) {
-    return (
-        <div id="content" className={"table" + (props.wide ? ' wide' : '')}>
-            <div className="cell">
-                <main className={props.mainClass ? ' '+props.mainClass : ''}>
-                    {props.children}
+class BoxedTrappingsComponent extends React.Component {
 
-                    <footer>
-                        Contao Manager v1.0
-                        <a href="https://manager.contao.org" target="_blank" className="support">Support</a>
-                    </footer>
-                </main>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            blurClass: ''
+        };
+
+        this.blurIn = this.blurIn.bind(this);
+        this.blurOut = this.blurOut.bind(this);
+    }
+
+    componentDidMount() {
+        eventhandler.on('displayTaskPopup', this.blurIn);
+        eventhandler.on('hideTaskPopup', this.blurOut);
+    }
+
+    componentWillUnmount() {
+        eventhandler.removeListener('displayTaskPopup', this.blurIn);
+        eventhandler.removeListener('hideTaskPopup', this.blurOut);
+    }
+
+    blurIn() {
+        this.setState({blurClass: ' blur-in'});
+    }
+
+    blurOut() {
+        this.setState({blurClass: ' blur-out'});
+    }
+
+    render() {
+        return (
+            <div id="content" className={"table" + (this.props.wide ? ' wide' : '') + this.state.blurClass}>
+                <div className="cell">
+                    <main className={this.props.mainClass ? ' ' + this.props.mainClass : ''}>
+                        {this.props.children}
+
+                        <footer>
+                            Contao Manager v1.0
+                            <a href="https://manager.contao.org" target="_blank" className="support">Support</a>
+                        </footer>
+                    </main>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default BoxedTrappingsComponent;
