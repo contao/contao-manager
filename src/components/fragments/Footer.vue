9cc50106aaaf53d9e35db0dd945d9ada4ac61f24
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-04-26 14:17:47 +0200

    Added bug report link to footer

--- src/components/fragments/Footer.vue
+++ src/components/fragments/Footer.vue
@@ -1,6 +1,9 @@
 <template>
     <footer v-once>
         Contao Manager @package_version@
-        <a href="https://docs.contao.org" target="_blank" class="help">Help</a>
+        <ul class="links">
+            <li><a href="https://docs.contao.org" target="_blank">Help</a></li>
+            <li><a href="https://github.com/contao/contao-manager/issues/new" target="_blank">Report a Problem</a></li>
+        </ul>
     </footer>
 </template>
