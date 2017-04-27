18b349ba035ae1b3453ece238ab22d60ee0fe8b9
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-04-27 16:46:19 +0200

    CS & dependency updates

--- api/Process/CommandLine.php
+++ api/Process/CommandLine.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of Contao Manager.
+ *
+ * Copyright (c) 2016-2017 Contao Association
+ *
+ * @license LGPL-3.0+
+ */
+
 namespace Contao\ManagerApi\Process;
 
 use Contao\ManagerApi\ApiKernel;
