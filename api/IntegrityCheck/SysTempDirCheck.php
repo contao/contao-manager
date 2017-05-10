beae9bad1089d9fea4574f50316988db4a8e0634
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-05-10 12:11:44 +0200

    Added integrity check for sys_temp_dir

---
+++ api/IntegrityCheck/SysTempDirCheck.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Contao Manager.
+ *
+ * Copyright (c) 2016-2017 Contao Association
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\ManagerApi\IntegrityCheck;
+
+use Crell\ApiProblem\ApiProblem;
+
+class SysTempDirCheck extends AbstractIntegrityCheck
+{
+    public function run()
+    {
+        if (false !== ($tmpfile = tempnam(sys_get_temp_dir(), '')) && is_writable($tmpfile)) {
+            return null;
+        }
+
+        return (new ApiProblem(
+            $this->trans('systempdir.title'),
+            'https://php.net/open_basedir'
+        ))->setDetail($this->trans('systempdir.detail'));
+    }
+}
