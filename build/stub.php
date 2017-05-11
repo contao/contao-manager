ee0c81c9ee6cbfb9b5956554a8c5e05385acdbc2
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-05-11 09:23:55 +0200

    Phar stub is now compatible with any PHP 5 version to show version error

--- build/stub.php
+++ build/stub.php
@@ -8,7 +8,7 @@ if (function_exists('ini_set')) {
 }
 
 if (version_compare(phpversion(), '5.5.9', '<')) {
-    die('You need at least PHP 5.5.9 to run the Contao Manager.');
+    die('You are using PHP '.phpversion().' but you need least PHP 5.5.9 to run the Contao Manager.');
 }
 
 if (!extension_loaded('Phar')) {
@@ -27,19 +27,15 @@ if (false !== ($suhosin = ini_get('suhosin.executor.include.whitelist'))) {
     }
 }
 
-$isDisabled = function ($value) {
-    return '' === $value || 0 === (int) $value || 'Off' === $value;
-};
-
-if (false !== ($multibyte = ini_get('zend.multibyte')) && !$isDisabled($multibyte)) {
+if (false !== ($multibyte = ini_get('zend.multibyte')) && '' !== $multibyte && 0 !== (int) $multibyte && 'Off' !== $multibyte) {
     $unicode = ini_get(version_compare(phpversion(), '5.4', '<') ? 'detect_unicode' : 'zend.detect_unicode');
 
-    if (!$isDisabled($unicode)) {
+    if ('' !== $unicode && 0 !== (int) $unicode && 'Off' !== $unicode) {
         die('The detect_unicode setting needs to be disabled in your php.ini.');
     }
 }
 
-unset($isDisabled, $multibyte, $unicode);
+unset($multibyte, $unicode);
 
 if ('cgi-fcgi' === php_sapi_name() && extension_loaded('eaccelerator') && ini_get('eaccelerator.enable')) {
     die('The PHP eAccelerator extension cannot handle .phar files.');
@@ -55,35 +51,37 @@ if ('cli' === PHP_SAPI) {
     /** @noinspection PhpIncludeInspection */
     require 'phar://contao-manager.phar/api/console';
 } else {
+    function rewrites() {
+        // The function argument is unreliable across servers, Nginx for example is always empty
+        list(,$url) = explode(basename(__FILE__), $_SERVER['REQUEST_URI'], 2);
+
+        if (strpos($url, '..')) {
+            return false;
+        }
+
+        if ('' === $url) {
+            header('Location: /'.basename(__FILE__).'/');
+            exit;
+        }
+
+        if ('/' === $url) {
+            return '/web/index.html';
+        }
+
+        if (0 === strpos($url, '/api/')) {
+            return '/web/api.php'.$url;
+        }
+
+        // rewrite everything to the public folder
+        return '/web'.$url;
+    }
+
     Phar::webPhar(
         'contao-manager.phar',
         'index.html',
         null,
         array(),
-        function () {
-            // The function argument is unreliable across servers, Nginx for example is always empty
-            list(,$url) = explode(basename(__FILE__), $_SERVER['REQUEST_URI'], 2);
-
-            if (strpos($url, '..')) {
-                return false;
-            }
-
-            if ('' === $url) {
-                header('Location: /'.basename(__FILE__).'/');
-                exit;
-            }
-
-            if ('/' === $url) {
-                return '/web/index.html';
-            }
-
-            if (0 === strpos($url, '/api/')) {
-                return '/web/api.php'.$url;
-            }
-
-            // rewrite everything to the public folder
-            return '/web'.$url;
-        }
+        'rewrites'
     );
 }
 
