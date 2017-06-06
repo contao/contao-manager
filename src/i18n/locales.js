a064a02bd9ed655665342879c8c902f5267d0f32
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-06-06 10:44:38 +0200

    Downloaded latest translations from Transifex

--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -1,8 +1,15 @@
+// Local and international language names from ISO 639-1 (Wikipedia)
+// https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+// Order by language code
+
 export default {
+    cs: 'čeština (Czech)',
+    de: 'Deutsch (German)',
     en: 'English',
-    cs: 'Czech',
-    es: 'Español',
-    fa: 'Farsi',
-    ja: 'Japanese',
-    ru: 'Russian',
+    es: 'Español (Spanish)',
+    fa: 'فارسی (Persian)',
+    ja: '日本語 (にほんご) (Japanese)',
+    nl: 'Nederlands (Dutch)',
+    ru: 'Русский (Russian)',
+    sr: 'српски језик (Serbian)',
 };
