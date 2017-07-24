f86e226cc43b393bccbcbf00f33d6aac9d62553f
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-07-24 17:36:42 +0200

    Make sure the update date has not been manipulated to the future

--- api/Controller/StatusController.php
+++ api/Controller/StatusController.php
@@ -248,6 +248,7 @@ TAG
 
         if ($this->managerConfig->has('last_update')
             && false !== ($lastUpdate = new \DateTime($this->managerConfig->get('last_update')))
+            && $lastUpdate <= time()
             && $lastUpdate > new \DateTime('-1 hour')
         ) {
             return false;
