36f5f22d67cff8ef4fc7320917d414397beefc98
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-02-03 21:35:42 +0100

    Disable install tool lock/unlock in safe mode

--- src/components/routes/maintenance/InstallTool.vue
+++ src/components/routes/maintenance/InstallTool.vue
@@ -1,28 +1,37 @@
 <template>
-    <section class="maintenance">
-        <div class="maintenance__inside">
-            <figure class="maintenance__image"><img src="../../../assets/images/logo.svg" /></figure>
+    <message-overlay :message="$t('ui.maintenance.safeMode')" :active="safeMode">
+        <section class="maintenance">
+            <div class="maintenance__inside">
+                <figure class="maintenance__image"><img src="../../../assets/images/logo.svg" alt="" /></figure>
 
-            <div class="maintenance__about">
-                <h1>{{ 'ui.maintenance.installTool.title' | translate }}</h1>
-                <p>{{ 'ui.maintenance.installTool.description' | translate }}</p>
+                <div class="maintenance__about">
+                    <h1>{{ 'ui.maintenance.installTool.title' | translate }}</h1>
+                    <p>{{ 'ui.maintenance.installTool.description' | translate }}</p>
+                </div>
+
+                <fieldset class="maintenance__actions">
+                    <button-group :label="$t('ui.maintenance.installTool.unlock')" type="primary" icon="lock" @click="unlock">
+                        <link-menu :items="advancedActions()" color="primary"/>
+                    </button-group>
+                </fieldset>
             </div>
-
-            <fieldset class="maintenance__actions">
-                <button-group :label="$t('ui.maintenance.installTool.unlock')" type="primary" icon="lock" @click="unlock">
-                    <link-menu :items="advancedActions()" color="primary"/>
-                </button-group>
-            </fieldset>
-        </div>
-    </section>
+        </section>
+    </message-overlay>
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+
+    import MessageOverlay from '../../fragments/MessageOverlay';
     import ButtonGroup from '../../widgets/ButtonGroup';
     import LinkMenu from '../../fragments/LinkMenu';
 
     export default {
-        components: { ButtonGroup, LinkMenu },
+        components: { MessageOverlay, ButtonGroup, LinkMenu },
+
+        computed: {
+            ...mapState(['safeMode']),
+        },
 
         methods: {
             advancedActions() {
