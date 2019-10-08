a8162cc8d6cf0383241b04590758209e4acdad7c
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-10-08 17:47:12 +0200

    Improve display of artifact packages

--- src/components/routes/Packages/LocalPackage.vue
+++ src/components/routes/Packages/LocalPackage.vue
@@ -7,17 +7,14 @@
         :update-only="updateOnly"
     >
         <template #release v-if="isUpload">
-            <input type="text" class="disabled" :title="$t('ui.package.uploadConstraint')" :value="data.version" disabled>
-            <p class="package__release-description package__version--release" v-if="isProvider">
-                <img src="../../../assets/images/lock.svg" width="24" height="24" alt=""/>
-                <strong>{{ $t('ui.package.providerTitle') }}</strong><br>
-                {{ $t('ui.package.providerText') }}
-            </p>
-            <p class="package__release-description package__version--release" v-else>
-                <img src="../../../assets/images/upload.svg" width="24" height="24" alt=""/>
-                <strong>{{ $t('ui.package.uploadedTitle') }}</strong><br>
-                {{ $t('ui.package.uploadedText') }}
-            </p>
+            <fieldset>
+                <input type="text" class="disabled" :title="$t('ui.package.privateTitle')" :value="$t('ui.package.private')" disabled>
+                <button class="widget-button widget-button--gear" :title="$t('ui.package.uploadConstraint')" disabled>{{ $t('ui.package.private') }}</button>
+            </fieldset>
+            <div class="package__version package__version--release" v-if="data.version">
+                <strong>{{ $t('ui.package.version', { version: data.version }) }}</strong>
+                <time :dateTime="data.time" v-if="data.time">({{ data.time | datimFormat }})</time>
+            </div>
         </template>
 
         <slot name="actions" slot="actions"/>
@@ -46,8 +43,6 @@
 
         computed: {
             ...mapState('packages', ['installed']),
-
-            isProvider: vm => vm.data.type === 'contao-provider',
 
             isUpload: vm => vm.data['installation-source'] === 'dist'
                 && vm.data.dist
