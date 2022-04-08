15c3259ace4a7f197b6650ca0752fbc84eedde31
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-04-08 20:36:14 +0200

    Drop obsolete route name

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,6 @@
 export default {
     discover: { name: 'discover' },
     packages: { name: 'packages' },
-    packagesSearch: { name: 'packages-search' },
     oauth: { name: 'oauth' },
     maintenance: { name: 'maintenance' },
 };
