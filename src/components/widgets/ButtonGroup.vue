1db6f16d034d4bdaff525c0d990cd44710b0e30f
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-04-13 17:54:28 +0200

    Support vue-router links in buttons

--- src/components/widgets/ButtonGroup.vue
+++ src/components/widgets/ButtonGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="button-group">
-        <loading-button :class="primaryClass" @click="e => $emit('click', e)" :disabled="disabled" :loading="loading">{{ label }}</loading-button>
+        <loading-button :href="href" :to="to" :class="primaryClass" @click="e => $emit('click', e)" :disabled="disabled" :loading="loading">{{ label }}</loading-button>
         <button :class="moreClass" @click="toggle" :disabled="moreDisabled"><svg fill="#FFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
         <div ref="group" :class="{ 'button-group__group': true, [`button-group__group--top`]: alignTop }" v-show="showGroup" tabindex="-1" @blur="close" @click="close">
             <slot/>
@@ -20,6 +20,8 @@
                 required: true,
             },
             type: String,
+            href: String,
+            to: [String, Object],
             icon: String,
             disabled: Boolean,
             loading: Boolean,
@@ -60,11 +62,11 @@
         methods: {
             open() {
                 this.showGroup = true;
-                this.$nextTick(() => this.$refs.group.focus());
+                this.$nextTick(() => this.$refs.group?.focus());
             },
 
             close() {
-                this.$refs.group.blur();
+                this.$refs.group?.blur();
                 setTimeout(() => {
                     this.showGroup = false;
                 }, 300);
