c6c238a601f95bbf50125c26216b548367f23588
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-08-19 23:17:03 +0200

    Fixed translation in database setup

--- src/components/setup/DatabaseConnection.vue
+++ src/components/setup/DatabaseConnection.vue
@@ -47,7 +47,11 @@
             <main class="setup__form setup__form--center" v-else v-bind:key="'confirmation'">
                 <div class="setup__fields setup__fields--center">
                     <svg class="setup__check" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M11,16.5L6.5,12L7.91,10.59L11,13.67L16.59,8.09L18,9.5L11,16.5Z" /></svg>
-                    <p class="setup__fielddesc">Successfully connected to database <i>{{ database }}</i> on <i>{{ server }}</i> ({{ currentServerVersion }}).</p>
+                    <i18n tag="p" path="ui.setup.database-connection.connected" class="setup__fielddesc">
+                        <template #database><i>{{ database }}</i></template>
+                        <template #server><i>{{ server }}</i></template>
+                        <template #version>({{ currentServerVersion }})</template>
+                    </i18n>
 
                     <p class="setup__fielddesc setup__warning" v-if="status && status.total > 0">{{ $tc(`ui.setup.database-connection.${currentState}`, status.total) }}</p>
                     <p class="setup__fielddesc" v-else>{{ $t('ui.setup.database-connection.noChanges') }}</p>
