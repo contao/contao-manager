2d894a9f7f882ef4606bc4afcfc63ffeab081fa9
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-13 23:17:10 +0100

    Added PhpScoper

---
+++ scoper.inc.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+$polyfillsBootstraps = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->files()
+            ->in(__DIR__.'/vendor/symfony/polyfill-*')
+            ->name('bootstrap*.php'),
+        false,
+    ),
+);
+
+$polyfillsStubs = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->files()
+            ->in(__DIR__.'/vendor/symfony/polyfill-*/Resources/stubs')
+            ->name('*.php'),
+        false,
+    ),
+);
+
+$symfonyDeprecationContracts = array_map(
+    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
+    iterator_to_array(
+        Finder::create()
+            ->files()
+            ->in(__DIR__.'/vendor/symfony/deprecation-contracts')
+            ->name('*.php'),
+        false,
+    ),
+);
+
+return [
+    'prefix' => '_ContaoManager',
+    'whitelist' => [
+        Finder::class,
+    ],
+    'exclude-namespaces' => [
+        'Symfony\Polyfill',
+        '/^Composer/',
+    ],
+    'exclude-constants' => [
+        '/^SYMFONY\_[\p{L}_]+$/',
+        '/^COMPOSER(\_[\p{L}_]+)?$/',
+    ],
+    'exclude-functions' => [
+        'trigger_deprecation',
+    ],
+    'exclude-files' => [
+        ...$polyfillsBootstraps,
+        ...$polyfillsStubs,
+        ...$symfonyDeprecationContracts,
+    ],
+
+    'patchers' => [
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('src/Reflector.php' !== $filePath) {
+                return $contents;
+            }
+
+            $originalContents = file_get_contents(__DIR__.'/src/Reflector.php');
+
+            $classPosition = mb_strpos($originalContents, 'final class Reflector');
+            $prefixedClassPosition = mb_strpos($contents, 'final class Reflector');
+
+            return sprintf(
+                '%s%s',
+                mb_substr($contents, 0, $prefixedClassPosition),
+                mb_substr($originalContents, $classPosition),
+            );
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if ('bin/php-scoper' !== $filePath) {
+                return $contents;
+            }
+
+            return str_replace(
+                '\\'.$prefix.'\Isolated\Symfony\Component\Finder\Finder::class',
+                '\Isolated\Symfony\Component\Finder\Finder::class',
+                $contents,
+            );
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            $files = ['vendor/symfony/dependency-injection/Loader/Configurator/Traits/ParentTrait.php', 'vendor/symfony/dependency-injection/Compiler/ResolveInstanceofConditionalsPass.php'];
+            if (!\in_array($filePath, $files, true)) {
+                return $contents;
+            }
+
+            $contents = str_replace(
+                [
+                    '$definition = \substr_replace($definition, \'53\', 2, 2);',
+                    '$definition = \substr_replace($definition, \'Child\', 44, 0);',
+                ],
+                [
+                    '$definition = \substr_replace($definition, \''.(53 + strlen($prefix.'\\')).'\', 2, 2);',
+                    '$definition = \substr_replace($definition, \'Child\', '.(44 + strlen($prefix.'\\')).', 0);'
+                ],
+                $contents
+            );
+
+            return $contents;
+        },
+        static function (string $filePath, string $prefix, string $contents): string {
+            if (!str_starts_with($filePath, 'vendor/composer/composer/src/Composer/Package')) {
+                return $contents;
+            }
+
+            $contents = str_replace("'$prefix\\\\", "'", $contents);
+
+            return $contents;
+        },
+    ],
+];
