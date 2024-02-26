558321a6029c516db44c3f53052ed65d8cb5c2fb
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-02-26 11:54:14 +0100

    Updated dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining'
+    '@vue/cli-plugin-babel/preset',
+    '@babel/preset-env',
   ]
 };
