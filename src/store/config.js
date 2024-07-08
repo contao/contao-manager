417cc7e3e9a665c18b1afc37a80acd963c6d03bf
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-07-08 18:38:08 +0200

    Revert unlocking config/manager

--- src/store/config.js
+++ src/store/config.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-param-reassign */
 
 import composer from './config/composer';
-import manager from './config/manager';
 
 export default {
     namespaced: true,
 
     modules: {
         composer,
-        manager
     },
 };
