efca0ef3b813ce47d3de90b12af16d6dc4ec4294
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-02-10 10:20:23 +0100

    Updated dependencies

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,15 @@ module.exports = {
     },
 
     chainWebpack: config => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: 'true',
+                __VUE_PROD_DEVTOOLS__: 'false',
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+            })
+            return definitions
+        })
+
         config
             .module
             .rule('images')
@@ -62,7 +71,7 @@ module.exports = {
                     ...options,
                     compilerOptions: {
                         compatConfig: {
-                            MODE: 2
+                            MODE: 3
                         }
                     }
                 }
