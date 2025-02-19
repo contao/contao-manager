e00967bc49c8a414ca5ab86304e520365ce7d512
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-02-19 13:15:22 +0100

    Request::all() never returns NULL for composerJson

--- api/Controller/Packages/CloudController.php
+++ api/Controller/Packages/CloudController.php
@@ -16,6 +16,7 @@ use Composer\Json\JsonFile;
 use Contao\ManagerApi\Composer\Environment;
 use Contao\ManagerApi\HttpKernel\ApiProblemResponse;
 use Contao\ManagerApi\Task\TaskManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,6 +31,7 @@ class CloudController
         private readonly Environment $environment,
         private readonly TaskManager $taskManager,
         private readonly Filesystem $filesystem,
+        private readonly LoggerInterface $logger,
     ) {
     }
 
@@ -63,19 +65,27 @@ class CloudController
             $lockFile = new JsonFile($this->filesystem->tempnam(sys_get_temp_dir(), md5(\Phar::running())));
             $lockFile->write($lock);
             $lockContent = $lockFile->read(); // Validates the JSON
+        } catch (\Throwable $throwable) {
+            $this->logger->error('Invalid composerLock for /api/packages/cloud.', ['composerLock' => $lock]);
 
-            if (null !== ($json = $request->request->all('composerJson'))) {
+            return ApiProblemResponse::createFromException($throwable);
+        }
+
+        try {
+            if ($request->request->has('composerJson') && [] !== ($json = $request->request->all('composerJson'))) {
                 $jsonFile = new JsonFile($this->filesystem->tempnam(sys_get_temp_dir(), md5(\Phar::running())));
                 $jsonFile->write($json);
                 $jsonFile->validateSchema(JsonFile::LAX_SCHEMA);
                 $this->environment->getComposerJsonFile()->write($jsonFile->read());
             }
-
-            // Only write after composer.json was validated
-            $this->environment->getComposerLockFile()->write($lockContent);
         } catch (\Throwable $throwable) {
+            $this->logger->error('Invalid composerJson for /api/packages/cloud.', ['composerJson' => $json, 'composerLock' => $lock]);
+
             return ApiProblemResponse::createFromException($throwable);
         }
+
+        // Only write after composer.json was validated
+        $this->environment->getComposerLockFile()->write($lockContent);
 
         return new JsonResponse($this->taskManager->createTask('composer/install', []));
     }
