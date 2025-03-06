8294c69676d963059675c9eef7b304c7983b94bf
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-03-06 10:12:05 +0100

    Decorate service instead of extending final class

--- api/EventListener/LocaleListener.php
+++ api/EventListener/LocaleListener.php
@@ -12,19 +12,43 @@ declare(strict_types=1);
 
 namespace Contao\ManagerApi\EventListener;
 
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\EventListener\LocaleListener as BaseLocaleListener;
 
-class LocaleListener extends BaseLocaleListener
+#[AsDecorator('locale_listener')]
+class LocaleListener implements EventSubscriberInterface
 {
+    public function __construct(private readonly BaseLocaleListener $inner)
+    {
+    }
+
+    public function setDefaultLocale(KernelEvent $event): void
+    {
+        $this->inner->setDefaultLocale($event);
+    }
+
     public function onKernelRequest(RequestEvent $event): void
     {
-        parent::onKernelRequest($event);
+        $this->inner->onKernelRequest($event);
 
         $request = $event->getRequest();
 
         if ($locale = $request->getPreferredLanguage()) {
             $request->setLocale($locale);
         }
+    }
+
+    public function onKernelFinishRequest(FinishRequestEvent $event): void
+    {
+        $this->inner->onKernelFinishRequest($event);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return BaseLocaleListener::getSubscribedEvents();
     }
 }
